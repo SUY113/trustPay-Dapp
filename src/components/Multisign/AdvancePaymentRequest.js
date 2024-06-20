219e2ae8d5b2936313c65e09df30890fc2a5cb06
Nguyễn Tạ Huy Hoàng <85854007+NguyenTaHuyHoang@users.noreply.github.com>
2024-06-20 13:56:45 +0700

    add multisign

--- src/components/Multisign/AdvancePaymentRequest.js
+++ src/components/Multisign/AdvancePaymentRequest.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate} from 'react-router-dom';
 import './AdvancePaymentRequest.css';
 
 function AdvancePaymentRequest() {
+  const [userName, setUserName] = useState('');
+  const [orgName, setOrgName] = useState('');
   const [requestID, setRequestID] = useState('');
   const [targetAccount, setTargetAccount] = useState('');
-  const [message, setMessage] = useState('');
+  const [evaluateRequestResult, setEvaluateRequestResult] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
+  const [finalizeRequestResult, setFinalizeRequestResult] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedUserName = localStorage.getItem('userName');
+    const storedOrgName = localStorage.getItem('orgName');
+    if (storedUserName && storedOrgName) {
+      setUserName(storedUserName);
+      setOrgName(storedOrgName);
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       await axios.post('http://localhost:3000/submit-request', {
+        userName : userName,
+        orgName : orgName,
         requestID: requestID,
         targetAccount: targetAccount,
-        message: message
       });
 
       setSuccessMessage('Request submitted successfully.');
@@ -28,22 +45,39 @@ function AdvancePaymentRequest() {
     }
   };
 
+
   const handleEvaluateRequest = async () => {
     try {
-      const res = await axios.post('http://localhost:3000/evaluate-request', { requestID });
-      setMessage(res.data.result);
+      const res = await axios.post('http://localhost:3000/evaluate-request', {userName : userName,
+        orgName : orgName, requestID: requestID });
+      setEvaluateRequestResult(res.data.result);
     } catch (error) {
-      setMessage(error.res.data.error);
+      console.error('Error:', error);
+      if (error.response && error.response.data) {
+        setEvaluateRequestResult(error.response.data.error);
+      } else {
+        setEvaluateRequestResult('An unexpected error occurred.');
+      }
     }
   };
 
   const handleFinalizeRequest = async () => {
     try {
-      const res = await axios.post('http://localhost:3000/finalize-request', { requestID });
-      setMessage(res.data.result);
+      const res = await axios.post('http://localhost:3000/finalize-request', { userName : userName,
+        orgName : orgName, requestID: requestID  });
+      setFinalizeRequestResult(res.data.result);
     } catch (error) {
-      setMessage(error.res.data.error);
+      console.error('Error:', error);
+      if (error.response && error.response.data) {
+        setFinalizeRequestResult(error.response.data.error);
+      } else {
+        setFinalizeRequestResult('An unexpected error occurred.');
+      }
     }
+  };
+
+  const handleBackClick = () => {
+    navigate('/dashboard-Staff');
   };
 
   return (
@@ -52,6 +86,14 @@ function AdvancePaymentRequest() {
       {errorMessage && <p className="error-message">{errorMessage}</p>}
       {successMessage && <p className="success-message">{successMessage}</p>}
       <form onSubmit={handleSubmit}>
+        <label>
+          Tên đăng nhập:
+          <input type="text" value={userName} readOnly />
+        </label>
+        <label>
+          Tổ chức:
+          <input type="text" value={orgName} readOnly />
+        </label>
         <label>
           Request ID:
           <input
@@ -70,21 +112,25 @@ function AdvancePaymentRequest() {
             required
           />
         </label>
-        <label>
-          Message:
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          />
-        </label>
         <button type="submit">Submit Request</button>
       </form>
       <div>
       <button className="evaluate-request-button" onClick={handleEvaluateRequest}>Evaluate Request</button>
       <button className="finalize-request-button" onClick={handleFinalizeRequest}>Finalize Request</button>
       </div>
+      {evaluateRequestResult && (
+        <div className="result-message">
+          <h3>Evaluate Request Result:</h3>
+          <p>{evaluateRequestResult}</p>
+        </div>
+      )}
+            {finalizeRequestResult && (
+        <div className="result-message">
+          <h3>Finalize Request Result:</h3>
+          <p>{finalizeRequestResult}</p>
+        </div>
+      )}
+      <button className="back-button" onClick={handleBackClick}>Back</button>
     </div>
   );
 }
