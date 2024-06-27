1730b79bb9b4c6214e98c7d3fa24ccb138d7d24b
NguyenTaHuyHoang <nthh01082002@gmail.com>
2024-06-28 06:58:45 +0700

    mint

--- src/components/Database/DashboardAccountant.js
+++ src/components/Database/DashboardAccountant.js
@@ -17,36 +17,10 @@ function DashboardAccountant() {
     useState(false);
   const [mintAmount, setMintAmount] = useState("");
   const navigate = useNavigate();
-  const [, setAccount] = useState("");
-  const [web3, setWeb3] = useState(null);
-
-  const BalanceOfETHABI = [
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "_ethTransferAddress",
-          type: "address",
-        },
-      ],
-      stateMutability: "nonpayable",
-      type: "constructor",
-    },
-    {
-      inputs: [],
-      name: "retrieveOwnerBalance",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-  ];
-  const BalanceOfETHABIAddress = "0xe582B3DF22aa33a7C8d0cF28C97f560f3f829aC0";
+  const [, setAccountEth] = useState("");
+  const [web3Eth, setWeb3Eth] = useState(null);
+  const [web3Contract, setWeb3Contract] = useState(null);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
     const storedUserName = localStorage.getItem("userName");
@@ -61,61 +35,141 @@ function DashboardAccountant() {
   }, [navigate]);
 
   useEffect(() => {
-    const loadWeb3 = async () => {
+    const loadWeb3Eth = async () => {
       if (typeof window.web3 !== "undefined") {
         const web3Instance = new Web3(window.web3.currentProvider);
-        setWeb3(web3Instance);
+        setWeb3Eth(web3Instance);
       } else {
         const web3Instance = new Web3(
           new Web3.providers.HttpProvider("http://localhost:8545")
         );
-        setWeb3(web3Instance);
+        setWeb3Eth(web3Instance);
       }
     };
-    loadWeb3();
+    loadWeb3Eth();
   }, []);
 
   useEffect(() => {
     const fetchAccountInfo = async () => {
-      if (web3) {
-        web3.eth.getAccounts((err, accounts) => {
+      if (web3Eth) {
+        web3Eth.eth.getAccounts((err, accounts) => {
           if (err) {
             console.error("Error fetching accounts:", err);
             return;
           }
-          setAccount(accounts[0]);
-
-          // web3.eth.getBalance(accounts[0], (err, balanceWei) => {
-          //   if (err) {
-          //     console.error("Error fetching balance:", err);
-          //     return;
-          //   }
-          //   const balanceInEther = Math.floor(
-          //     web3.fromWei(balanceWei, "ether")
-          //   );
-          //   setMintAmount(balanceInEther.toString());
-          // });
-
-          const BalanceOfETHInstance = web3.eth
-            .contract(BalanceOfETHABI)
-            .at(BalanceOfETHABIAddress);
-
-          BalanceOfETHInstance.retrieveOwnerBalance((err, balanceWei) => {
+          setAccountEth(accounts[0]);
+          // get balance
+          web3Eth.eth.getBalance(accounts[0], (err, balanceWei) => {
             if (err) {
               console.error("Error fetching balance:", err);
               return;
             }
             const balanceInEther = Math.floor(
-              web3.fromWei(balanceWei, "ether")
+              web3Eth.fromWei(balanceWei, "ether")
             );
-            setMintAmount(balanceInEther.toString());
+            setBalance(balanceInEther);
+            //setMintAmount(balance.toString());
           });
         });
       }
     };
-
     fetchAccountInfo();
-  }, [web3]);
+  }, [web3Eth]);
+
+  useEffect(() => {
+    const loadWeb3Contract = async () => {
+      try {
+        const web3Instance = new Web3(
+          new Web3.providers.HttpProvider("http://localhost:5000")
+        );
+        setWeb3Contract(web3Instance);
+      } catch (error) {
+        console.error("Failed to connect to the node:", error);
+      }
+    };
+    loadWeb3Contract();
+  }, []);
+
+  useEffect(() => {
+    const loadContract = async () => {
+      if (web3Contract && web3Contract.eth) {
+        try {
+          web3Contract.eth.defaultAccount = await web3Contract.eth.accounts[0];
+          const BalanceOfETHABI = [
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "value",
+                  type: "uint256",
+                },
+              ],
+              name: "setBalance",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "getBalance",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ];
+
+          const BalanceOfETHBytecode =
+            "608060405234801561001057600080fd5b5061012f806100206000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c806312065fe0146037578063fb1669ca146051575b600080fd5b603d6069565b6040516048919060c2565b60405180910390f35b6067600480360381019060639190608f565b6072565b005b60008054905090565b8060008190555050565b60008135905060898160e5565b92915050565b60006020828403121560a057600080fd5b600060ac84828501607c565b91505092915050565b60bc8160db565b82525050565b600060208201905060d5600083018460b5565b92915050565b6000819050919050565b60ec8160db565b811460f657600080fd5b5056fea2646970667358221220368d41917c426e40ea507231b18db146b0116d1581fc3781d82c3432448aa97e64736f6c63430008030033";
+
+          const BalanceOfETH = new web3Contract.eth.Contract(BalanceOfETHABI);
+          const deployedContract = await BalanceOfETH.deploy({
+            data: BalanceOfETHBytecode,
+          })
+            .send({ from: web3Contract.eth.defaultAccount })
+            .on("receipt", (receipt) => {
+              console.log(
+                "Contract deployed successfully:",
+                receipt.contractAddress
+              );
+            })
+            .on("error", (error) => {
+              console.error("Error deploying contract:", error);
+              throw error; // Rethrow after logging
+            });
+
+          const contractAddress = deployedContract.options.address;
+          const myContract = new web3Contract.eth.Contract(
+            BalanceOfETHABI,
+            contractAddress
+          );
+
+          await myContract.methods
+            .setBalance(balance)
+            .send({ from: web3Contract.eth.defaultAccount })
+            .on("receipt", (receipt) => {
+              console.log("Transaction successful:", receipt);
+            })
+            .on("error", (error) => {
+              console.error("Error in transaction:", error);
+              throw error; // Rethrow after logging
+            });
+
+          const result = await myContract.methods.getBalance().call();
+          console.log("Balance:", result.toString());
+          setMintAmount(result.toString());
+        } catch (error) {
+          console.error("Error interacting with the contract:", error);
+        }
+      }
+    };
+    loadContract();
+  }, [web3Contract]);
 
   const getChannelForOrg = (orgName) => {
     switch (orgName) {
